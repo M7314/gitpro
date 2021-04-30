0d182b80bb2c2d45b373fa9f83d747cf1f113103
吴梦凯 <1774617314@qq.com>
2021-04-30 14:49:50 +0800

    提交输出信息3

---
+++ src/main/java/com.atguigu/test.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @Date 2021/4/30 14:48
+ * @Description TODO
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("123123123");
+    }
+}
